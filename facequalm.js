62fced0de3c029026240c87f67c2d408e4e82772
dggriffin <donggriffin@gmail.com>
2015-10-14 23:28:01 -0400

    Adding base account functionality.

--- facequalm.js
+++ facequalm.js
@@ -1,11 +1,35 @@
 Router.configure({
     layoutTemplate: 'main',
-    notFoundTemplate : '404'
+    notFoundTemplate: '404'
+});
+
+Router.onBeforeAction(function() {
+    if (!Meteor.userId()) {
+        this.render('register');
+    } else {
+        this.next();
+    }
 });
 
 if (Meteor.isClient) {
     // counter starts at 0
-    Session.setDefault('counter', 0);
+    Template.main.helpers({
+        signedInAs : function() {
+            var user = Meteor.user();
+
+            if (user) {
+                if (user.username) {
+                    return user.username;
+                } else if (user.profile && user.profile.name) {
+                    return user.profile.name;
+                } else if (user.emails && user.emails[0]) {
+                    return user.emails[0].address;
+                } else {
+                    return "Signed In";
+                }
+            }
+        }
+    });
 }
 
 if (Meteor.isServer) {
