822e7b9be9aef2923722fe1b38d0401c5054d428
Gal Shenar <gshenar@gmail.com>
2015-10-29 19:37:33 -0700

    newfaces

--- client/templates/home/home.js
+++ client/templates/home/home.js
@@ -65,30 +65,6 @@
     Template.inviteFriends.events({
         'click #start-game': function() {
             var selectedFriends = $('input[type=checkbox]:checked');
-            var randomImages = ["10-reasons-denzel-washington-is-badass-561610606-may-11-2012-600x400.jpg",
-                "52af99b77d9317858d34eb519130cc2d.jpg",
-                "5f7e2c6433ee5811e092f80d8a437545.jpg",
-                "FunnyFaces201007121648120034.jpg",
-                "Jessie J's facial expressions on The Voice.jpg",
-                "amiley.jpg",
-                "c4ded2ce3f83183cc7557f22c8d35cda_fullsize.jpg",
-                "celeb-expressions-141.jpg",
-                "celebrity_funny_faces_pictures.jpg",
-                "funny-celebrity-faces-23.jpg",
-                "funny-celebrity-faces-5.jpg",
-                "funny-obama-faces-strange-0.jpg",
-                "jim-carreys-best-facial-expressions-of-the-90s-1-348-1358448944-5_big.jpg",
-                "katy-perry-funny-face-7.jpg",
-                "miley-cyrus-funny-faces-14-1377697818-large-article-0.jpg",
-                "opera-facials-0.jpg",
-                "rexfeatures_1822822ag.jpg",
-                "s-KANYE-WEST-DOESNT-SMILE-large.jpg"];
-
-                var path = '/facepics/'; // default path here
-                var num = Math.floor( Math.random() * randomImages.length );
-                var img = randomImages[ num ];
-                var imgStr = path + img;
-
                 Meteor.call('createGame', Meteor.userId(), function(error, results) {
                     if (error) {
                         console.log(error.reason)
