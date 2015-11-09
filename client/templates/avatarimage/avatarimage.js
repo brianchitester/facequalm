a9dbfff65cd58bd2e9c0cdf4db05ff39ac86bb72
Gal Shenar <gshenar@gmail.com>
2015-11-08 16:49:00 -0800

    fix avatar images to show users avatar

--- client/templates/avatarimage/avatarimage.js
+++ client/templates/avatarimage/avatarimage.js
@@ -1,6 +1,12 @@
 if (Meteor.isClient) {
     Template.avatarimage.helpers({
         avatarUrl: function() {
+        	if(this.userName) {
+	        	return Meteor.users.findOne({                                     // 5
+	            	username: this.userName                                        // 6
+	        	}).avatarUrl;
+        	}
+            //If no username is passed in use current user
             var user = Meteor.user();
             if (user && user.avatarUrl) {
                 return user.avatarUrl;
